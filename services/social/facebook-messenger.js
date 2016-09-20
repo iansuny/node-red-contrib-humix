d62f569b1fa9e3e51e35e08f0709d52e4602a857
iansuny <iansuny@yahoo.com.tw>
2016-09-21 00:17:04 +0800

    fb messenger in now are able to receive attachment message

--- services/social/facebook-messenger.js
+++ services/social/facebook-messenger.js
@@ -27,43 +27,39 @@ module.exports = function(RED) {
         senderId = event.sender.id;
         node.log("Receiving messages from sender: " + senderId);
 
-        if (event.message) {
-          if (event.message.text){
-            messageText = event.message.text;
-            node.log("Receiving message text: " + messageText);
+        if (event.message && event.message.text) {
+          messageText = event.message.text;
+          node.log("Receiving message text: " + messageText);
 
-            node.send({
-              facebook: {
-                channel: FB_CHANNEL,
-                peerId: senderId || undefined,
-                msgType: 'text',
-                msgText: messageText,
-                msgAttachmentURL: null,
-                msgTemplate: null
-              }
-            });
-          }else if (event.message.attachments){
-            for (j = 0; j < event.message.attachments.length; j++){
-              msgType = event.message.attachments[j].type;
-              msgAttachmentURL = event.message.attachments[j].payload.url;
-              node.log("Receiving message " + msgType + ": " + messageUrl);
-
-              node.send({
-                facebook: {
-                  channel: FB_CHANNEL,
-                  peerId: senderId || undefined,
-                  msgType: msgType,
-                  msgText: null,
-                  msgAttachmentURL: msgAttachmentURL,
-                  msgTemplate: null
-                }
-              });
-
-              // Todo : receive location message
-              
+          node.send({
+            facebook: {
+              channel: FB_CHANNEL,
+              peerId: senderId || undefined,
+              msgType: 'text',
+              msgText: messageText,
+              msgImageURL: null,
+              msgTemplate: null
             }
-          }
+          });
+        }else{
+          msgType = event.message.attachments[0].type;
+          msgAttachmentURL = event.message.attachments[0].payload.url;
+          node.log("Receiving message " + msgType + ": " + messageUrl);
+
+          node.send({
+            facebook: {
+              channel: FB_CHANNEL,
+              peerId: senderId || undefined,
+              msgType: msgType,
+              msgText: null,
+              msgAttachmentURL: msgAttachmentURL,
+              msgTemplate: null
+            }
+          });
         }
+
+        // Todo : receive location message
+
       }
 
       res.sendStatus(200);
